746f363ff9cc925f87ad2dd06994004b790e2bf0
henry <henrywrightman@gmail.com>
2021-10-10 22:47:12 -0400

    refine cron example

--- example-cron.js
+++ example-cron.js
@@ -1,11 +1,14 @@
 const cron = require('node-cron');
 const distributor = require('./src/services/distributor');
 const models = require('./components/models');
+const blockchain = require('./src/services/blockchain');
 
 cron.schedule('*/60 * * * *', async () => {
 	const transactions = []; // acquire transactions somehow, e.g database or queue
 	await distributor.constructMerkleRoot(transactions, async (merkle) => {
     	const tx = models.transactions.build("SetMerkleRoot", { epochNumber: merkle.epoch, merkleRoot: merkle.root });
     	//.. execute tx
+    	const result = await blockchain.callTransaction(tx);
+    	console.log(result);
 	});
 });
